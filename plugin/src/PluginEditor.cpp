6ea9d6c3bbe76cce1048bc5d0ea202e3a412243b
Arthur <arthur@freye.in>
2023-11-13 20:48:50 +0100

    it goes now better

--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -8,7 +8,7 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (500, 300);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
